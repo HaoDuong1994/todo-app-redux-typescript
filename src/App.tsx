8bb4ca396aa144a8164944c637c5cb68f213eb79
HaoDuong1994 <hao.dv5370@gmail.com>
2025-04-18 18:11:03 +0700

    update UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import "./App.css";
 import AddTaskPage from "../src/component/addTaskPage";
 function App() {
   return (
-    <>
+    <div className="p-5">
       <AddTaskPage />
-    </>
+    </div>
   );
 }
 
--- src/component/addTaskPage.tsx
+++ src/component/addTaskPage.tsx
@@ -5,6 +5,7 @@ import { addTodo, completedTodo } from "../redux/Slice/todoSlice";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import { TiPencil } from "react-icons/ti";
+import moment from "moment";
 const AddTaskPage: React.FC = () => {
   //Config title
   const [title, setTitle] = useState<string>("");
@@ -26,19 +27,49 @@ const AddTaskPage: React.FC = () => {
       isCompleted: false,
     };
     dispatch(addTodo(todoItem));
+    setTitle("");
   };
-  console.log(todoList);
   //Handle Completed
   const handleCompleted = (idTodo: string): void => {
     dispatch(completedTodo(idTodo));
   };
   return (
-    <div>
-      <div>
-        <div>
+    <div className="flex flex-col h-screen items-center">
+      <div className="text-2xl font-bold">
+        Your have <span>{todoList.length}</span> task today
+      </div>
+      <div className="w-full flex flex-col items-center gap-8">
+        {todoList.map((todoItem) => {
+          return (
+            <div
+              className={
+                todoItem.isCompleted
+                  ? "w-[80%] grid grid-cols-5 line-through items-start"
+                  : "w-[80%] grid grid-cols-5 items-start"
+              }>
+              <TiPencil />
+              <div className="break-words whitespace-normal w-[160px]">
+                {todoItem.title}
+              </div>
+              <div>Pending</div>
+              <div>{moment(todoItem.createDate).format("LLL")}</div>
+              <button
+                onClick={() => {
+                  handleCompleted(todoItem.id);
+                }}
+                className="bg-green-500 text-white w-[120px] px-4 py-2 rounded hover:bg-green-600 cursor-pointer">
+                Completed
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border border-solid h-24 w-[40%] flex flex-col items-center space-y-3">
+        <div className="w-full">
           <input
+            value={title}
             onChange={handleInputTodo}
-            className="border border-solid"
+            className="w-full border border-solid"
             type="text"
             placeholder="Add task here"
           />
@@ -48,39 +79,8 @@ const AddTaskPage: React.FC = () => {
           onClick={() => {
             handleAddTask();
           }}>
-          Add task
+          Add
         </button>
-      </div>
-      <div>
-        <div className="flex justify-center space-x-1 items-center">
-          <TiPencil />
-          <div>Quét nhà</div>
-          <div>Pending</div>
-          <div>Date</div>
-          <button className="bg-green-500 p-4">Completed</button>
-        </div>
-        {todoList.map((todoItem) => {
-          return (
-            <div
-              className={
-                todoItem.isCompleted
-                  ? "flex justify-center space-x-1 items-center line-through"
-                  : "flex justify-center space-x-1 items-center"
-              }>
-              <TiPencil />
-              <div>{todoItem.title}</div>
-              <div>Pending</div>
-              <div>{todoItem.createDate}</div>
-              <button
-                onClick={() => {
-                  handleCompleted(todoItem.id);
-                }}
-                className="bg-green-500 p-4">
-                Completed
-              </button>
-            </div>
-          );
-        })}
       </div>
     </div>
   );
